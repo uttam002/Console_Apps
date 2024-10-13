9803e9afcaa6d2375caa4b0f5d625894234f4b14
HP <HP@UTTAM>
2024-10-13 13:35:34 +0530

    Add project Contact Book

---
+++ Contact BOOK in C#/Contact BOOK/Models/Contact.cs
@@ -0,0 +1,28 @@
+﻿namespace ContactBook.Models
+{
+    public class Contact
+    {
+        public string Name { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public string Email { get; private set; }
+
+        public Contact(string name, string phoneNumber, string email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public void UpdateContact(string name, string phoneNumber, string email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name,-20} | {PhoneNumber,-15} | {Email,-30}";
+        }
+    }
+}
