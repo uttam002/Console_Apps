816ba30b59ce6bacac6d4776fc4a0bece8cfc1dd
HP <HP@UTTAM>
2024-10-13 13:58:27 +0530

    Add code for BlackjackGame

---
+++ Black-jack Game in C#/Black-jack Game/Models/Card.cs
@@ -0,0 +1,21 @@
+﻿namespace BlackjackGame.Models
+{
+    public class Card
+    {
+        public string Suit { get; set; }
+        public string Rank { get; set; }
+        public int Value { get; set; }
+
+        public Card(string rank, string suit, int value)
+        {
+            Rank = rank;
+            Suit = suit;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Rank} of {Suit}";
+        }
+    }
+}
